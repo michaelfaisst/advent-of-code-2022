1868cfb2f24f56ed3ed08411ee601fdeb32067ff
Michael Faißt <michael@codenite.at>
2022-12-15 23:06:55 +0100

    uncomment day 15 a solving

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ mod day15;
 fn main() {
     let input: String = read_file(15, false);
 
-    // solve(day15::a::solve, &input);
+    solve(day15::a::solve, &input);
     solve(day15::b::solve, &input);
 }
